841fc567c092f5f147340fecf0ecaca079366d17
themartincox <martin@postino.cc>
2025-08-08 12:42:37 +0100

    buttons

--- pear-tree-dental/src/components/SmileQuizClient.tsx
+++ pear-tree-dental/src/components/SmileQuizClient.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useMemo, useRef, useState } from "react"
-import { Button } from "@/components/button"
+import { Button } from "@/components/ui/button"
 
 // Questions and scoring map
 const QUESTIONS = [
